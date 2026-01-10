37861fe5ab6dea0981d946f048a9a85ff8616a65
Bibek Pathak <bibek.pathak@selu.edu>
2026-01-10 11:52:24 -0600

    Build sussesful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 
 import { ProjectsSection } from "@/components/ui/projects-section";
 import { CertificatesSection } from "@/components/ui/certificates-section";
-import { ContactSection } from "@/components/ui/contact-section";
 
-import { NewHero } from "@/components/ui/new-hero";
+import { ContactSection } from "@/components/ui/contact-section";
+import { NewHero }from "@/components/ui/new-hero";
 
 
 
@@ -11,7 +11,7 @@ import { NewHero } from "@/components/ui/new-hero";
 export default function HomePage() {
   return (
     <>
-    <NewHero />
+      <NewHero />
       <ProjectsSection />
       
       <CertificatesSection />
